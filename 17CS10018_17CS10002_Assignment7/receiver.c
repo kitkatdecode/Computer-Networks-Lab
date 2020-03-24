130056a21d4c891a1ae067a307f4307d63f536fb
vivekvardhanadepu <vivekvardhanadepu839@outlook.com>
2020-03-24 16:15:34 +0530

    sender and receiver partly done[needs tweaking]

--- 17CS10018_17CS10002_Assignment7/receiver.c
+++ 17CS10018_17CS10002_Assignment7/receiver.c
@@ -1,82 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+/* R E C E I V E R */
+
 #include <string.h>
-#include <sys/types.h>
 #include "rsocket.h"
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
 #define PORT 70037
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 1
 
 int main(){
     int sockfd;
-    struct sockaddr_in servaddr;
+    struct sockaddr_in sendaddr,recvaddr;
 
     // creating sockfd
-    sockfd = socket(AF_INET, SOCK_DGRAM,0);
+    sockfd = socket(AF_INET, SOCK_MRP,0);
     if(sockfd < 0){
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&recvaddr, 0, sizeof(recvaddr));
 
     // Server info
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = INADDR_ANY;
+    recvaddr.sin_family = AF_INET;
+    recvaddr.sin_port = htons(PORT);
+    recvaddr.sin_addr.s_addr = INADDR_ANY;
 
-    int n;
-    int recv_msg_len;
-    char buffer[BUFFER_SIZE];
+    if(r_bind(sockfd, (const struct sockaddr *) &recv_addr, sizeof(recv_addr)) < 0){
+		perror("binding failed");
+		exit(EXIT_FAILURE);
+	}
 
-    // sending filename and creating a new file if hello is received
-    char filename[BUFFER_SIZE];
-    printf("Enter the filename : ");
-    scanf("%s", filename);
-    sendto(sockfd, (const char *)filename, strlen(filename), 0, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-    
-    recv_msg_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, NULL, NULL);
-    buffer[recv_msg_len] = '\0';
-    printf("SERVER: %s\n", buffer);
-    fflush(stdout);
-    
-    char not_found[] = "NOTFOUND ";
-    
-    if(!strcmp(buffer, strcat(not_found, filename))){
-        printf("Error: File %s Not Found\n", filename);
-        return 0;
-    }
-    
-    FILE *file_ptr;
-    file_ptr = fopen("recv_file.txt", "w");
+    char buffer;
 
-    // sending requests and printing in file
-    char word[20];
-    int i = 1;
-    while(1){
-        memset(word, 0, 20);
-        sprintf(word, "WORD%d", i);
-        sendto(sockfd, (const char *)word, strlen(word), 0, 
-                        (const struct sockaddr *)&servaddr, sizeof(servaddr));
-
-        memset(buffer, 0, BUFFER_SIZE);
-        recv_msg_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, NULL, NULL);
-        buffer[recv_msg_len] = '\0';
-        printf("SERVER: %s\n", buffer);
-        fflush(stdout);
-        
-        if(strcmp(buffer,"END"))fprintf(file_ptr, "%s\n", buffer);
-        else
-        {
-            fclose(file_ptr);
-            close(sockfd);
-            return 0;
-        }
-        i++;
-    }
-
+    while(recvfrom(sockfd, &buffer,1, 0, 
+                            (const struct sockaddr *) &send_addr, sizeof(send_addr)) == 1)
+        printf("%c",buffer);
+    r_close(sockfd);
     return 0;
 }
--- 17CS10018_17CS10002_Assignment7/sender.c
+++ 17CS10018_17CS10002_Assignment7/sender.c
@@ -1,21 +1,18 @@
-/* S E R V E R */
+/* S E N D E R */
 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
 #include <string.h> 
-#include <sys/types.h> 
 #include <rsocket.h> 
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
 
 #define PORT 70036
-#define BUFFERSIZE 1024
+#define RECV_PORT 70037
+#define MSGSIZE 1024
 
 
 int main(){
 	int sockid;
-	struct sockaddr_in ser_addr, cli_addr;
+	struct sockaddr_in send_addr, recv_addr;
 
 
 	if((sockid = r_socket(AF_INET, SOCK_MRP, 0)) < 0){
@@ -23,74 +20,46 @@ int main(){
 		exit(EXIT_FAILURE);
 	}
 
-	memset(&ser_addr, 0, sizeof(ser_addr));
+	memset(&send_addr, 0, sizeof(send_addr));
 
-	ser_addr.sin_family = AF_INET;
-	ser_addr.sin_addr.s_addr = INADDR_ANY;
-	ser_addr.sin_port = htons(PORT);
+    // sender's info
+	send_addr.sin_family = AF_INET;
+	send_addr.sin_addr.s_addr = INADDR_ANY;
+	send_addr.sin_port = htons(PORT);
 
-	if(bind(sockid, (const struct sockaddr *) &ser_addr, sizeof(ser_addr)) < 0){
+    memset(&recvaddr, 0, sizeof(recvaddr));
+
+    // receiver's info
+    recvaddr.sin_family = AF_INET;
+    recvaddr.sin_port = htons(RECV_PORT);
+    recvaddr.sin_addr.s_addr = INADDR_ANY;
+
+	if(r_bind(sockid, (const struct sockaddr *) &send_addr, sizeof(send_addr)) < 0){
 		perror("binding failed");
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Server Running.....waiting for client's message...\n");
+	printf("Sender Running\n");
 	fflush(stdout);
 
-	int n;
-	socklen_t len;
-	char buffer[BUFFERSIZE];
-	FILE *fp;
+    char msg[MSGSIZE];
+    memset(msg, 0, MSGSIZE);
+    printf("Enter the message : ");
+    scanf("%s", msg);
 
-	len = sizeof(cli_addr);
+    printf("sending <=100 characters\n");
 
-	while(1){
-		memset(buffer, 0, BUFFERSIZE);
-		n = recvfrom(sockid, buffer, BUFFERSIZE, 0, (struct sockaddr *)&cli_addr, &len);
-		buffer[n] = '\0';
-		printf("CLIENT: %s\n", buffer);
-		fflush(stdout);
+    // sending message character wise
+    int msglen = strlen(msg);
+    if(msglen>100)
+        msglen = 100;
+    sendto(sockid, (const char *)msg[0], strlen(msg[0]), 0,
+                    (const struct sockaddr *)&recvaddr, sizeof(recvaddr));
+    for(int i=1;i<msglen;i++){
+        sendto(sockid, (const char *)msg[i], strlen(msg[i]), 0,
+                    (const struct sockaddr *)&recvaddr, sizeof(recvaddr));
+    }
 
-		fp = fopen(buffer, "r");
-		
-		if(fp == NULL){
-			char err_msg[] = "NOTFOUND ";
-			strcat(err_msg, buffer);
-
-			printf("SERVER: File not found\n");
-			fflush(stdout);
-
-			sendto(sockid, err_msg, strlen(err_msg), 0, (struct sockaddr *)&cli_addr, len);
-		}
-	
-		else {
-			fscanf(fp, "%s", buffer);
-			sendto(sockid, buffer, strlen(buffer), 0, (struct sockaddr *)&cli_addr, len);
-			
-			printf("SERVER: File found, sending to Client\n");
-			fflush(stdout);
-			
-			while(1){
-				n = recvfrom(sockid, buffer, BUFFERSIZE, 0, (struct sockaddr *)&cli_addr, &len);
-				buffer[n] = '\0';
-
-				printf("CLIENT: %s\n", buffer);
-				fflush(stdout);
-				memset(buffer, 0, BUFFERSIZE);
-				
-				if(fscanf(fp, "%s", buffer) == EOF) break;
-
-				sendto(sockid, buffer, strlen(buffer), 0, (struct sockaddr *)&cli_addr, len);
-
-				if(!strcmp(buffer,"END"))break;
-			}
-			fclose(fp);
-			printf("SERVER: File sent to the Client\n");
-			fflush(stdout);
-		}
-	}
-
-	close(sockid);
-	return 0;
-
+    r_close(sockid);
+    return 0;
 }
